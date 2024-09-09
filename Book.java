b36155b01d0434542348f93a7e8d96037b1d5ea9
asjadafrahim04 <asjadafrahim.04@gmail.com>
2024-09-09 08:46:32 +0600

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter8;
+
+/**
+ *
+ * @author Lenovo S540
+ */
+public enum Book {
+    JHTP("Java How to Program", "2015"),
+    CHTP("C How to Program", "2013"),
+    IW3HTP("Internet & World Wide Web How to Program", "2012"),
+    CPPHTP("C++ How to Program", "2014"),
+    VBHTP("Visual Basic How to Program", "2014"),
+    CSHARPHTP("Visual C# How to Program", "2014");
+    private final String title;
+    private final String copyrightYear;
+
+    Book(String title, String copyrightYear) {
+        this.title = title;
+        this.copyrightYear = copyrightYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCopyrightYear() {
+        return copyrightYear;
+    }
+}
---
+++ EmployeeTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter8;
+
+/**
+ *
+ * @author Lenovo S540
+ */
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+        Date birth = new Date(7, 24, 1949);
+        Date hire = new Date(3, 12, 1988);
+        Employee employee = new Employee("Bob", "Blue", birth, hire);
+        System.out.println(employee);
+
+    }
+}
